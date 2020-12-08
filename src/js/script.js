89474aa96e8a3758fa69acc33c6b986428e41eb5
Mateusz Rozenkiewicz <rodzyn22@gmail.com>
2020-12-08 22:25:53 +0100

    add modal to script.js, style.scss

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,40 @@ document.querySelector('.hamburger').addEventListener('click', function(e){
     e.preventDefault();
     toggleMenu();
 });
+
+function closeModal(){
+    document.getElementById('overlay').classList.remove('show')
+}
+
+/* Zamkniecie modala przez przycisk "x" */
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn){
+    btn.addEventListener('click', function(e){
+    e.preventDefault();
+    closeModal();
+    })
+})
+/* Zamkniecie modala przez nacisniecie na tło */
+    document.querySelector('#overlay').addEventListener('click', function(e){
+        if(e.target === this){
+            closeModal();
+        }
+    })
+/* Zamkniecie modala przez przycisk "ESC" */
+    document.addEventListener('keyup', function(e){
+        if(e.kayCode === 27){
+            closeModal();
+        }
+    })
+
+/* Otwieranie Modala (zamkniecie wszystkich, owarcie wskazanego przez overlay) */
+
+function openModal(modal){
+    document.querySelectorAll('#overlay > *').forEach(function(modal){
+        modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+}
+
+
+
